842e03ce039a5932e8f670b4c45f94085da4a731
mobile Apps4you <azizmaghraoui130013@gmail.com>
2025-06-18 20:55:57 +0100

    try public conf for main

--- recommandation_model.py
+++ recommandation_model.py
@@ -145,4 +145,4 @@ def recommend_professionals(client_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
